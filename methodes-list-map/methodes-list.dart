45e03549ed884b189dc7c4c95804bbbe4ab2be0d
THE-FIVE <tialaonahousse@gmail.com>
2021-03-02 17:18:33 +0000

    up

--- methodes-list-map/methodes-list.dart
+++ methodes-list-map/methodes-list.dart
@@ -56,26 +56,35 @@ void main(List<String> args) {
   print("__________________");
 
   // 9 - inserer plusieurs éléments dans la liste de fruits à partir d'une position(index) - insertAll();
-
+  print("____ insertAll() _____");
   fruits.insertAll(6, ['Ananas','Anone','Aronia','Avocat','Banane','Bergamote','Bibasse','Bigaradier','Bleuet','Brugnon','Cacahuète','Caïmite','Calamondin','Camerise','Canneberge','Carambole','Cassis','CédratCerise']);
   print(fruits);
   print("__________________");
 
   // 10 - Retourne un nouvel Itérable avec des éléments qui sont créés en appelant f sur chaque élément de cet Itérable dans l'ordre d'itération.
   //var elementfruits = fruits;
-  print("__________________");
-  var r = fruits.map((e) => null);
-  print(r);
+  print("____ map() _____");
+  var r = fruits.map((e) => e + '_1' ).toList();
+  if (r != ' ') {
+    print(r);
+  } else {
+    print('erreur');
+  }
   print("__________________");
 
   // 11 - where()
-  print("__________________");
-  fruits.where((element) => false);
+  print("____ where() _____");
+  var w = fruits.where((items) => items).toList();
+  if (w == 'Amande' || w == 'Ananas') {
+    print(w);
+  }
   print("__________________");
 
   // 12 - firstwhere()
-  print("__________________");
-  fruits.firstWhere((element) => false);
+  print("________ firstwhere() __________");
+  var fw = fruits.firstWhere((items1) => items1);
+  print(fw);
+
   print("__________________");
   
   // 13 - list.from()
