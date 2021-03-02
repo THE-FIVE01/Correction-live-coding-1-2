9f5122b48ba3581ff562cdee20802f8021ae9d5a
THE-FIVE <tialaonahousse@gmail.com>
2021-03-02 16:36:17 +0000

    update

---
+++ methodes-list-map/methodes.dart
@@ -0,0 +1,91 @@
+
+void main(List<String> args) {
+  
+  //  List fruits = ['Abricot','Açaï','Airelle','Alkékenge','Amande','Ananas','Anone','Aronia','Avocat','Banane','Bergamote','Bibasse','Bigaradier','Bleuet','Brugnon','Cacahuète','Caïmite','Calamondin','Camerise','Canneberge','Carambole','Cassis','CédratCerise'];
+  // Création d'une liste de fruits vide
+  List fruits = [];
+
+  // 1 - Ajouter un élément dans la liste de fruits - add();
+  fruits.add('Abricot');
+  print(fruits);
+  print("__________________");
+
+  // 2 - Ajouter plusieurs éléments dans la liste de fruits - addAll();
+  fruits.addAll(['Açaï','Airelle','Alkékenge','Amande']);
+  print(fruits);
+  print("__________________");
+
+  // 3 - mettre lés élemnt du de la liste en ordre - sort();
+  //fruits.sort();
+  print(fruits);
+  print("__________________");
+
+  // 4 - Vérifier si une liste est vide - isEmpty()  retoune true oou false;
+  var verifFruitsVide = fruits.isEmpty;
+  print(verifFruitsVide);
+  print("__________________");
+
+  // 5 - vérifier si la liste de fruits contient un élément donné - contains();
+  String element = 'Avocat';
+  var verifElementDansFRuits = fruits.contains(element);
+  print(verifElementDansFRuits);
+  if(verifElementDansFRuits) {
+    print("$element est dans la liste de fruits");
+    print("__________________");
+  }else {
+    print("$element n'est pas dans la liste de fruits");
+  }
+  
+  // 6 - Trouver l'index d'un élément donné dans une liste - indexOf();
+  String element2 = 'Abricot';
+  var index = fruits.indexOf(element2);
+  print(index);
+  print("$element2 est situé à l'index $index dans la liste de fruits");
+  print("__________________");
+
+  // 7 - Parcourir tout les éléments d'une liste - forEach();
+  fruits.forEach((element) { print(element);});
+
+  // 8 - inserer un element dans la liste à partir d'une position(index) avec - insert();
+  //var t = fruits.length;
+  print("____ insert() _____");
+  //print("Inserer un element a la position : ");
+  //print("position $t");
+  fruits.insert(0, 'element');
+  print(fruits);
+  print("__________________");
+
+  // 9 - inserer plusieurs éléments dans la liste de fruits à partir d'une position(index) - insertAll();
+
+  fruits.insertAll(6, ['Ananas','Anone','Aronia','Avocat','Banane','Bergamote','Bibasse','Bigaradier','Bleuet','Brugnon','Cacahuète','Caïmite','Calamondin','Camerise','Canneberge','Carambole','Cassis','CédratCerise']);
+  print(fruits);
+  print("__________________");
+
+  // 10 - Retourne un nouvel Itérable avec des éléments qui sont créés en appelant f sur chaque élément de cet Itérable dans l'ordre d'itération.
+  //var elementfruits = fruits;
+  print("__________________");
+  var r = fruits.map((e) => null);
+  print(r);
+  print("__________________");
+
+  // 11 - where()
+  print("__________________");
+  fruits.where((element) => false);
+  print("__________________");
+
+  // 12 - firstwhere()
+  print("__________________");
+  fruits.firstWhere((element) => false);
+  print("__________________");
+  
+  // 13 - list.from()
+
+  
+
+
+
+
+
+
+
+}
--- projets-mois-1/sim-robot2/lib/mechants.dart
+++ projets-mois-1/sim-robot2/lib/mechants.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 
 import 'classe_robot.dart';
 
-class Mechants {
+class Mechants extends Robot{
   
   // Propriétés 
 
@@ -23,16 +23,16 @@ class Mechants {
 
   void presentation () {
     print("____________________________________________________");
-    print("Bonjour je m'appelle ${this.mechantName} je suis un robot méchant. J'ai ${this.vie} points de vie et je me déplace à ${this.vitesseMax} cases par seconde. Je suis à la case de coordonnées ${this.position} .");
+    print("Bonjour je m'appelle ${this.mechantName} je suis un robot méchant. J'ai ${this.vie} points de vie et je me déplace à ${this.vitesseMax} cases par seconde. J'ai ${this.pointsDommage} point(s) de dommage - degat à distance ${this.degatDistance}.");
   }
 
   void attaquerCible(Mechants mechants, Robot robot) {
-    print("Le robot méchant ${mechants.mechantName} avec ${mechants.pointsDommage} point(s) de dommage -> attaque le robot gentille ${robot.robotName} : Nouveau points de Vie : ${robot.vie}");
+    print("Le robot méchant ${mechants.mechantName} avec ${mechants.pointsDommage} point(s) de dommage -> attaque le robot gentille ${robot.robotName} : points de Vie : ${robot.vie}");
     int mechantCoups = this.pointsDommage;
     robot.vie = this.vie = this.vie - mechantCoups;
     this.vie =robot.vie;
     print(".............");
-    print("le robot gentille ${robot.robotName} après avoir reçue un coups de ${mechants.pointsDommage} point(s) de dommage du robot méchant ${mechants.mechantName} perds $mechantCoups de vie -> Vie ${robot.robotName} : ${robot.vie}  ");
+    print("le robot gentille ${robot.robotName} après avoir reçue un coups de ${mechants.pointsDommage} point(s) de dommage du robot méchant ${mechants.mechantName} perds $mechantCoups de vie -> new point(s) de Vie ${robot.robotName} : ${robot.vie}  ");
     print("_____________");
     
   }
