699bb876f8122ae48d713da232f2a75880f63aad
THE-FIVE <tialaonahousse@gmail.com>
2021-03-10 18:21:04 +0000

    up

--- projets-flutter/app/lib/main.dart
+++ projets-flutter/app/lib/main.dart
@@ -14,14 +14,186 @@ class MyApp extends StatelessWidget {
       
       title: 'App StatelessWidget',
       theme: ThemeData(
-        primarySwatch: Colors.deepOrange
       ),
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: Home2(),
     );
   }
 }
 
+// Class Home2 - Code ------------------------------------
+
+class Home2 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Container(
+            child: Icon(
+              Icons.arrow_back_ios_rounded,
+              color: Colors.grey,
+            ),
+          ), 
+          
+          onPressed: () => print('Arrow back bouton')
+        ),
+        elevation: 0,
+        backgroundColor: Colors.white,
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey, width: 0.5),
+              borderRadius: BorderRadius.circular(10)
+            ),
+              child: Icon(
+                Icons.bookmark_rounded,
+                color: Colors.grey,
+              ),
+            ),
+          
+        ],
+      ),
+
+      body: Container(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Container(
+                width: 350,
+                height: 200,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 0.5),
+                  borderRadius: BorderRadius.circular(30),
+                  image: DecorationImage(image: AssetImage("images/3.png")),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 20),
+            Text(
+              'Duplex apartement',
+              style: TextStyle(
+                //color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            SizedBox(height: 2),
+        
+            Row(
+              children: [
+                  Text(
+                    'Stockton, New Hampshire',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 13,
+                      //fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 70),
+                    child: Icon(
+                      Icons.star,
+                      color: Colors.yellow[600],
+                    ),
+                  ),
+        
+                  Container(
+                    margin: EdgeInsets.only(left: 10),
+                    child: Text(
+                        '4.8',
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.bold
+                        ),
+                    ),
+                  ),
+                
+                  Container(
+                    margin: EdgeInsets.only(left: 5),
+                    child: Text(
+                        '(256 Reviews)',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 13,
+                        ),
+                    ),
+                  ),
+              ],
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Icon(
+                  Icons.single_bed,
+                  color: Colors.grey,
+                   size: 20,
+                ),
+                Container(
+                    margin: EdgeInsets.only(left: 10),
+                    child: Text(
+                        '5',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold
+                        ),
+                    ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 15),
+                  child: Icon(
+                    Icons.slow_motion_video_outlined,
+                    color: Colors.grey,
+                     size: 20,
+                  ),
+                ),
+                Container(
+                    margin: EdgeInsets.only(left: 10),
+                    child: Text(
+                        '2',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold
+                        ),
+                    ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 15),
+                  child: Icon(
+                    Icons.camera_alt_outlined,
+                    color: Colors.grey,
+                     size: 20,
+                  ),
+                ),
+                Container(
+                    margin: EdgeInsets.only(left: 10),
+                    child: Text(
+                        '1',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold
+                        ),
+                    ),
+                ),
+              ],
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Class Home - Code ------------------------------------
 class Home extends StatelessWidget {
   
   @override
