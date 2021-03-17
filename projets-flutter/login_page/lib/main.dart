1b9b77ac325c5b7ac1c7bc3344814523b8cc91a7
THE-FIVE <tialaonahousse@gmail.com>
2021-03-17 09:54:48 +0000

    Add login_page

---
+++ projets-flutter/login_page/lib/main.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Login screen'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Scaffold(
+        // appBar: AppBar(
+        //   title: Text(widget.title),
+        // ),
+        body: SingleChildScrollView(
+          child: Container(
+            constraints: BoxConstraints(
+              maxHeight: MediaQuery.of(context).size.height,
+              maxWidth: MediaQuery.of(context).size.width,
+            ),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.deepOrange[400],
+                  Colors.deepOrange[600],
+                  // Colors.deepOrange[600],
+                  // Colors.deepOrange[400],
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter
+              ), 
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 36.0, horizontal: 24.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 46.0,
+                            fontWeight: FontWeight.w800
+                          ),
+                        ),
+                        SizedBox(height: 10.0,),
+                        Text(
+                          "Enter to a beautiful World",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22.0,
+                            fontWeight: FontWeight.w300
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 5,
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40), // Arrondir le coté gauche
+                        topRight: Radius.circular(90) // Arrondir le coté droit
+                      ), 
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          TextField(
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                                borderSide: BorderSide.none // masque les bordures
+                              ),
+                              filled: true,
+                              fillColor: Color(0xFFe7edeb),
+                              hintText: "E-mail",
+                              prefixIcon: Icon(
+                                Icons.email,
+                                color: Colors.deepOrange[400], // Couleur de l'icone e-mail
+                              )
+                            ),
+                          ),
+                          SizedBox(height:20.0,),
+                          TextField(
+                            obscureText: true, // masquer le mot de passe saisie
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                                borderSide: BorderSide.none // masque les bordures
+                              ),
+                              filled: true,
+                              fillColor: Color(0xFFe7edeb),
+                              hintText: "Password",
+                              prefixIcon: Icon(
+                                Icons.lock,
+                                color: Colors.deepOrange[400], // Couleur de l'icone e-mail
+                              )
+                            ),
+                          ),
+                          SizedBox(height: 10.0,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                "Forget your password",
+                                style: TextStyle(
+                                  color: Colors.deepOrange[800],
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 50.0,),
+                          Container(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: () {
+
+                              }, 
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.deepOrange[400]
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 15.0),
+                                child: Text(
+                                  "Login",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+    
+  }
+}
