bfeab2d8d827b7f6f3b909f1dfc67ef807001037
THE-FIVE <tialaonahousse@gmail.com>
2021-04-07 15:00:06 +0000

    Up clima

---
+++ clima/lib/main2.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() async{
+  runApp(MyApp());
+  await  getWeatherInfos();
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: MyHomePage(title: 'WEATHER'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Future getWeatherInfos() async {
+  var url = Uri.parse("https://community-open-weather-map.p.rapidapi.com/weather");
+  var reponse = await http.get(url);
+  var body = json.decode(reponse.body);
+  try {
+    if (reponse.statusCode == 200) {
+      
+    } else {
+    }
+    
+  } catch (e) {
+    print("Erreur: $e");
+  }
+  
+  
+  
+  
+}
--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,4 +1,7 @@
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,17 +9,59 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+   Location location = Location();
+   await location.getCurrentLocation();
+   print(location.latitude);
+   print(location.longitude);
+   
+    
+  }
+
+  void getData() async {
+    var url = Uri.parse('api.openweathermap.org/data/2.5/weather?lat=35(&lon=139&appid=440687dd78810303564297d54f70cc26');
+    http.Response response = await http.get(url);
+    print(response.body);
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    }else {
+      print(response.statusCode);
+    }
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    super.deactivate();
+    getLocation();
+  }
+  
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
+      // body: Center(
+      //   child: ElevatedButton(
+      //     onPressed: () {
+      //       //Get the current location
+      //       getLocation();
+      //     },
+      //     child: Text('Get Location'),
+      //     style: ElevatedButton.styleFrom(
+      //       primary: Colors.blue
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
