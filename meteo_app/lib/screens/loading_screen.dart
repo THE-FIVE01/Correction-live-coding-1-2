304550466b139a25e5993ab954a8c23cda1fcfd1
THE-FIVE <tialaonahousse@gmail.com>
2021-04-13 17:06:03 +0000

    Up project meto_app

--- meteo_app/lib/screens/loading_screen.dart
+++ meteo_app/lib/screens/loading_screen.dart
@@ -3,6 +3,8 @@ import 'package:meteo_app/services/location.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+const apiKeys = '440687dd78810303564297d54f70cc26';
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
@@ -25,13 +27,22 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void getData() async {
     var url = Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=440687dd78810303564297d54f70cc26');
+        'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=$apiKeys');
     var response = await http.get(url);
     var data = jsonDecode(response.body);
 
     if (response.statusCode == 200) {
-      dynamic dataFinal = data;
-      print(dataFinal);
+      // dynamic dataFinal = data;
+      // print(data);
+
+      int condition = data['weather'][0]['id'];
+      double temperature = data['main']['temp'];
+      String cityName = data['name'];
+
+      print(temperature);
+      print(condition);
+      print(cityName);
+      
     } else {
       print(response.statusCode);
     }
@@ -39,7 +50,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    getData();
+    // getData();
 
     return Scaffold(
         // body: Center(
