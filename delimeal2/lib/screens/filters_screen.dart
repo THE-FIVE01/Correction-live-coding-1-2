ecfb57c2096c5d27c1583d0f82503a2e96d18743
THE-FIVE <tialaonahousse@gmail.com>
2021-05-26 17:00:50 +0000

    update 2 delimeal 2

---
+++ delimeal2/lib/screens/filters_screen.dart
@@ -0,0 +1,26 @@
+import 'package:delimeal2/widgets/main_drawer.dart';
+import 'package:flutter/material.dart';
+
+class FiltersScreen extends StatelessWidget {
+
+  static const routeName = '/filters-screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Your Filters",
+        ),
+      ),
+      drawer: MainDrawer(),
+      body: Container(
+        child: Center(
+          child: Text(
+            "Text",
+          ),
+        ),
+      ),
+    );
+  }
+}
--- delimeal2/lib/screens/tabs_screen.dart
+++ delimeal2/lib/screens/tabs_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './categories_screen.dart';
 import './favorites_screen.dart';
+import '../widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
 
@@ -9,37 +10,88 @@ class TabsScreen extends StatefulWidget {
 }
 class _TabsScreenState extends State<TabsScreen>{
 
+  List<Map<String, Object>> _pages = [
+    {'page': CategoriesScreen(), 'title': 'Categories'},
+    {'page': FavoritesScreen(), 'title': 'Your Favorites'},
+    
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      initialIndex: 0,
-      child: Scaffold(
+    return Scaffold(
         appBar: AppBar(
           centerTitle: true,
           title:Text(
-             "Meals",
-          ),
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category), 
-                text: "Categories",
-              ),
-              Tab(
-                icon: Icon(Icons.star), 
-                text: "Favorites",
-              ),
-            ],
+             _pages[_selectedPageIndex]['title'],
           ),
         ),
-        body: TabBarView(
-          children: [
-            CategoriesScreen(),
-            FavoritesScreen()
+        drawer: MainDrawer(),
+        body: _pages[_selectedPageIndex]['page'],
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Theme.of(context).primaryColor,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Theme.of(context).accentColor,
+          currentIndex: _selectedPageIndex,
+          type: BottomNavigationBarType.shifting,
+          onTap: _selectPage,
+          items: [
+            BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.category, ),
+              label: 'Categories',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              label: 'Favorites'
+            ),
           ],
         ),
-      ),
     );
   }
 }
+
+// class _TabsScreenState extends State<TabsScreen>{
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return DefaultTabController(
+//       length: 2,
+//       initialIndex: 0,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           centerTitle: true,
+//           title:Text(
+//              "Meals",
+//           ),
+//           bottom: TabBar(
+//             tabs: [
+//               Tab(
+//                 icon: Icon(Icons.category), 
+//                 text: "Categories",
+//               ),
+//               Tab(
+//                 icon: Icon(Icons.star), 
+//                 text: "Favorites",
+//               ),
+//             ],
+//           ),
+//         ),
+//         body: TabBarView(
+//           children: [
+//             CategoriesScreen(),
+//             FavoritesScreen()
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
