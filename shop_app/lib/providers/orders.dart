4d35bd81afd662209f878dbaa1f954ea29b58a90
THE-FIVE <tialaonahousse@gmail.com>
2021-07-06 16:25:31 +0000

    Update shop-app project

--- shop_app/lib/providers/orders.dart
+++ shop_app/lib/providers/orders.dart
@@ -29,6 +29,27 @@ class Orders with ChangeNotifier {
   Future<void> fetchAndSetOrders() async {
     final url = Uri.parse("https://shop-app-7a29c-default-rtdb.firebaseio.com/orders.json");
     final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if (extractedData == null) {
+      return;
+    }
+    extractedData.forEach((orderId, orderData) { 
+      loadedOrders.add(OrderItem(
+        id: orderId,
+        amount: orderData["amount"],
+        dateTime: DateTime.parse(orderData["dateTime"]),
+        products: (orderData["products"] as List<dynamic>).map((item) => CartItem(
+          id: item['id'],
+          price: item['price'],
+          quantity: item['quantity'],
+          title: item['title']
+        )).toList()
+      ),
+      );
+    });
+
+    _orders = loadedOrders;
 
     print(json.decode(response.body));
   }
--- shop_app/lib/screens/orders_screens.dart
+++ shop_app/lib/screens/orders_screens.dart
@@ -12,6 +12,22 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
+  var _isLoading = false;
+
+  @override
+  void initState() {
+    Future.delayed(Duration.zero).then((_) async {
+      setState(() {
+        _isLoading = true;
+      });
+      await  Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+       setState(() {
+        _isLoading = false;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final orderData = Provider.of<Orders>(context);
@@ -23,7 +39,7 @@ class _OrdersScreenState extends State<OrdersScreen> {
         ),
       ),
       drawer: AppDrawer(),  
-      body: ListView.builder(
+      body: _isLoading? CircularProgressIndicator() : ListView.builder(
         itemCount: orderData.orders.length,
         itemBuilder: (ctx, i) => OrderItem(
           orderData.orders[i],
